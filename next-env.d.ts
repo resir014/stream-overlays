6ad7e64af66347094e65b2d258b6e1c774d3eb64
Resi Respati <resir014@gmail.com>
2019-09-26 19:26:49 +0700

    refactored dts files

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,2 +1,20 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
+
+// CSS Modules
+declare module '*.css' {
+  interface ClassNames {
+    [className: string]: string
+  }
+  const classNames: ClassNames
+  export default classNames
+}
+
+// CSS Modules (.scss)
+declare module '*.scss' {
+  interface ClassNames {
+    [className: string]: string
+  }
+  const classNames: ClassNames
+  export default classNames
+}
