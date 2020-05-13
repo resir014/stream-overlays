c8054a2b3135547b155a9fb533c89d591c6e77d1
Resi Respati <resir014@gmail.com>
2020-05-13 13:11:11 +0700

    handle API error

--- pages/api/flightsim.ts
+++ pages/api/flightsim.ts
@@ -1,12 +1,22 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 
 const flightsim = async (_: NextApiRequest, res: NextApiResponse) => {
-  const response = await fetch('http://localhost:51011/overlay.json', {
-    mode: 'no-cors',
-    headers: { 'Content-Type': 'text/plain' }
-  })
-  const data = await response.text()
-  res.status(200).json(data)
+  try {
+    const response = await fetch('http://localhost:51011/overlay.json', {
+      mode: 'no-cors',
+      headers: { 'Content-Type': 'text/plain' }
+    })
+    const data = await response.text()
+    res.status(200).json(data)
+  } catch (err) {
+    console.error(err)
+    res.status(500).json({
+      status: 'error',
+      data: {
+        message: err.message
+      }
+    })
+  }
 }
 
 export default flightsim
