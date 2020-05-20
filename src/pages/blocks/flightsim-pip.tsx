12a116c6bece82c599627f57718ebd9103adca67
Resi Respati <resir014@gmail.com>
2020-05-20 17:21:52 +0700

    flightsim PIP page

---
+++ src/pages/blocks/flightsim-pip.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react'
+import { NextPage } from 'next'
+
+import LayoutRoot from 'components/layout/LayoutRoot'
+import ContentBlock from 'components/stream-blocks/ContentBlock'
+
+interface FooterPageProps {
+  errors?: Error['message']
+}
+
+const FlightsimPIPPage: NextPage<FooterPageProps> = () => {
+  return (
+    <LayoutRoot isTransparent>
+      <ContentBlock style={{ height: 376 }} title="Be Right Back..." />
+    </LayoutRoot>
+  )
+}
+
+export default FlightsimPIPPage
