77cf7da8d3ad3a214327381445f525b8377a650f
Resi Respati <resir014@gmail.com>
2020-05-20 20:51:59 +0700

    fixed full width bug

--- src/components/prestream/PrestreamSection.tsx
+++ src/components/prestream/PrestreamSection.tsx
@@ -4,6 +4,7 @@ const PrestreamSection = styled('div')`
   display: grid;
   grid-template-columns: auto 600px;
   grid-gap: 24px;
+  width: 100%;
   height: 100%;
   max-height: 700px;
   align-self: center;
