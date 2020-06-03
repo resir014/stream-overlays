234e8951d654edac08282d6b70285217c81f97e5
Resi Respati <resir014@gmail.com>
2020-06-03 17:55:23 +0700

    add subgoal widget styles

---
+++ templates/subgoal/subgoal.js
@@ -0,0 +1,36 @@
+// @ts-check
+
+/**
+ * @typedef {Object} GoalEvent
+ */
+
+const $title = document.getElementById('title')
+const $goalCurrent = document.getElementById('goal-current')
+const $goalTotal = document.getElementById('goal-total')
+const $goalEndDate = document.getElementById('goal-end-date')
+
+/**
+ * Events will be sent when someone subscribed.
+ * @param {GoalEvent} obj
+ */
+function goalLoad(obj) {
+  // obj.detail will contain information about the current goal
+  // this will fire only once when the widget loads
+  console.log(obj.detail)
+  $title.innerHTML = obj.detail.title
+  $goalCurrent.innerText = obj.detail.amount.current
+  $goalTotal.innerText = obj.detail.amount.target
+  $goalEndDate.innerText = obj.detail.to_go.ends_at
+}
+
+/**
+ * @param {GoalEvent} obj
+ */
+function goalEvent(obj) {
+  // obj.detail will contain information about the goal
+  console.log(obj.detail)
+  $goalCurrent.innerText = obj.detail.amount.current
+}
+
+document.addEventListener('goalLoad', goalLoad)
+document.addEventListener('goalEvent', goalEvent)
