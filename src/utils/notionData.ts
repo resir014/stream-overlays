38c2c0808318a2d87439af04007d787896331c72
Resi Respati <resir014@gmail.com>
2020-07-08 00:10:24 +0700

    also check stream detail from previous day
    Sometimes, streams can go past midnight, which means `.find()` will
    return empty the next day. Also check yesterday's stream details so that
    no empty detail exists.

--- src/utils/notionData.ts
+++ src/utils/notionData.ts
@@ -1,5 +1,6 @@
 import useSWR from 'swr'
 import format from 'date-fns/format'
+import subDays from 'date-fns/subDays'
 
 import { NotionData } from 'interfaces/types'
 
@@ -8,13 +9,21 @@ import fetch from './fetch'
 // https://github.com/splitbee/notion-api-worker
 const airtableAPIURL = `https://notion-api.splitbee.io/v1/table/${process.env.NEXT_PUBLIC_NOTION_STREAMS_TABLE_ID}`
 
-const currentDate = format(new Date(), 'yyyy-MM-dd')
+const currentDate = new Date()
+const currentFormattedDate = format(currentDate, 'yyyy-MM-dd')
+
+// need to check 1 day prior just in case
+const yesterdayFormattedDate = format(subDays(currentDate, 1), 'yyyy-MM-dd')
+
+function isTodayOrYesterday(notionData: NotionData) {
+  return notionData.Date === currentFormattedDate || notionData.Date === yesterdayFormattedDate
+}
 
 export async function fetchNotionData(): Promise<NotionData | null> {
   try {
     const schedule: NotionData[] = await fetch(airtableAPIURL)
 
-    return schedule?.find(record => record.Date === currentDate) || null
+    return schedule?.find(isTodayOrYesterday) || null
   } catch (err) {
     // eslint-disable-next-line no-console
     console.error(err)
