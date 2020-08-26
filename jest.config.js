0ac8446edd36f3c5b81ebf8ab90365f32b326b54
Resi Respati <resir014@gmail.com>
2020-08-26 23:27:07 +0700

    add Jest (#38)

---
+++ jest.config.js
@@ -0,0 +1,27 @@
+const TEST_REGEX = '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|js?|tsx?|ts?)$'
+
+module.exports = {
+  collectCoverageFrom: [
+    '**/*.{js,jsx,ts,tsx}',
+    '!**/*.d.ts',
+    '!**/node_modules/**',
+    '!**/.next/**'
+  ],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  testRegex: TEST_REGEX,
+  transform: {
+    '^.+\\.(jsx?|js?|tsx?|ts?)?$': 'babel-jest',
+    '^.+\\.css$': '<rootDir>/config/jest/cssTransform.js'
+  },
+  transformIgnorePatterns: ['/node_modules/', '^.+\\.module\\.(css|sass|scss)$'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  moduleNameMapper: {
+    '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
+    '^interfaces/(.*)$': '<rootDir>/src/interfaces/$1',
+    '^modules/(.*)$': '<rootDir>/src/modules/$1',
+    '^pages/(.*)$': '<rootDir>/src/pages/$1',
+    '^styles/(.*)$': '<rootDir>/src/styles/$1',
+    '^utils/(.*)$': '<rootDir>/src/utils/$1'
+  }
+}
