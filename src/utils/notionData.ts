ba74dbf808d1b86bdb7cbdad1171ab804b3350f7
Resi Respati <resir014@gmail.com>
2020-08-27 19:33:11 +0700

    updated notion filtering

--- src/utils/notionData.ts
+++ src/utils/notionData.ts
@@ -1,37 +1,21 @@
 import useSWR from 'swr'
-import format from 'date-fns/format'
-import subDays from 'date-fns/subDays'
 
 import { NotionData } from 'interfaces/types'
 
 import fetch from './fetch'
 
 // https://github.com/splitbee/notion-api-worker
-const airtableAPIURL = `https://notion-api.splitbee.io/v1/table/${process.env.NEXT_PUBLIC_NOTION_STREAMS_TABLE_ID}`
-
-const currentDate = new Date()
-const currentFormattedDate = format(currentDate, 'yyyy-MM-dd')
-
-// need to check 1 day prior just in case
-const yesterdayFormattedDate = format(subDays(currentDate, 1), 'yyyy-MM-dd')
+const notionAPIURL = `https://notion-api.splitbee.io/v1/table/${process.env.NEXT_PUBLIC_NOTION_STREAMS_TABLE_ID}`
 
 function isCurrentStream(notionData: NotionData) {
   return notionData['Current Stream'] === true
 }
 
-function isToday(notionData: NotionData) {
-  return notionData.Date === currentFormattedDate
-}
-
-function isYesterday(notionData: NotionData) {
-  return notionData.Date === yesterdayFormattedDate
-}
-
 export async function fetchNotionData(): Promise<NotionData | null> {
   try {
-    const schedule: NotionData[] = await fetch(airtableAPIURL)
+    const schedule: NotionData[] = await fetch(notionAPIURL)
 
-    return schedule?.find(isCurrentStream || isToday || isYesterday) || null
+    return schedule?.find(isCurrentStream) || null
   } catch (err) {
     // eslint-disable-next-line no-console
     console.error(err)
@@ -40,7 +24,7 @@ export async function fetchNotionData(): Promise<NotionData | null> {
 }
 
 export function useNotionData(initialData?: NotionData): NotionData | undefined {
-  const { data, error } = useSWR<NotionData | null>(airtableAPIURL, fetchNotionData, {
+  const { data, error } = useSWR<NotionData | null>(notionAPIURL, fetchNotionData, {
     initialData,
     refreshInterval: 10000
   })
