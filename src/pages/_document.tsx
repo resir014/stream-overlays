2e0a4eaa6e19d02875b44f75d1eaa54db93a5f94
Resi Respati <resir014@gmail.com>
2020-12-15 16:21:48 +0700

    fix type errors

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,23 +1,25 @@
 /* eslint-disable react/no-danger */
 import * as React from 'react'
 import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document'
-import { extractCritical } from 'emotion-server'
+import createEmotionServer from '@emotion/server/create-instance'
+import emotionCache from '~/utils/emotionCache'
+
+const { extractCritical } = createEmotionServer(emotionCache)
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const initialProps = await Document.getInitialProps(ctx)
-    const styles = extractCritical(initialProps.html)
+    const { css, ids } = extractCritical(initialProps.html)
     return {
       ...initialProps,
-      styles: (
-        <>
-          {initialProps.styles}
-          <style
-            data-emotion-css={styles.ids.join(' ')}
-            dangerouslySetInnerHTML={{ __html: styles.css }}
-          />
-        </>
-      )
+      styles: [
+        initialProps.styles,
+        <style
+          key="emotion-css"
+          data-emotion-css={ids.join(' ')}
+          dangerouslySetInnerHTML={{ __html: css }}
+        />
+      ]
     }
   }
 
