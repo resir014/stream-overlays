a002f372e3a6e58610275dfe5667dfc26c609269
Resi Respati <resir014@gmail.com>
2020-12-31 23:08:14 +0700

    add bottom-bar special for NYE

---
+++ src/pages/blocks/bottom-bar-nye.tsx
@@ -0,0 +1,14 @@
+import { NextPage } from 'next'
+import * as React from 'react'
+import OverlayRoot from '~/components/overlay/OverlayRoot'
+import BottomBar from '~/modules/bottom-bar/BottomBar'
+
+const BottomBarNYEPage: NextPage = () => {
+  return (
+    <OverlayRoot>
+      <BottomBar variant="prestream" />
+    </OverlayRoot>
+  )
+}
+
+export default BottomBarNYEPage
