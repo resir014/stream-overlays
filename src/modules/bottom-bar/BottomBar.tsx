34a1677434653627ec4062ed63836dd6481c8d3a
Resi Respati <resir014@gmail.com>
2021-01-01 21:25:12 +0700

    minor bottombar fix

--- src/modules/bottom-bar/BottomBar.tsx
+++ src/modules/bottom-bar/BottomBar.tsx
@@ -1,7 +1,7 @@
 import dynamic from 'next/dynamic'
 import { transparentize } from 'polished'
 import * as React from 'react'
-import { Box, colors } from '@resir014/chungking-react'
+import { Box, BoxProps, colors } from '@resir014/chungking-react'
 import BottomBarSocialLinks from './BottomBarSocialLinks'
 import BottomBarEvents from './BottomBarEvents'
 
@@ -9,18 +9,18 @@ const BottomBarClock = dynamic(() => import('./BottomBarClock'), {
   ssr: false
 })
 
-interface BottomBarProps {
+interface BottomBarProps extends BoxProps {
   variant?: 'default' | 'prestream' | 'nye'
 }
 
-const BottomBar: React.FC<BottomBarProps> = ({ variant }) => {
+const BottomBar: React.FC<BottomBarProps> = ({ variant, ...rest }) => {
   const hideClock = React.useMemo(() => variant === 'prestream', [variant])
   const nyeVariant = React.useMemo(() => variant === 'nye', [variant])
 
   return (
     <Box
       display="grid"
-      gridTemplateRows={hideClock ? '40px 48px' : '40px 24px'}
+      gridTemplateRows={hideClock || nyeVariant ? '40px 48px' : '40px 24px'}
       gridTemplateColumns="auto 320px"
       gridTemplateAreas={`
       "events clock"
@@ -31,6 +31,7 @@ const BottomBar: React.FC<BottomBarProps> = ({ variant }) => {
       backgroundColor={transparentize(0.1, colors.black)}
       color="white"
       maxHeight={hideClock ? 88 : 64}
+      {...rest}
     >
       <BottomBarEvents />
       {hideClock || nyeVariant ? <BottomBarSocialLinks /> : <BottomBarClock />}
