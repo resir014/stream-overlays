d720f3a196056417cd9007283db2f31f44cc1f37
Resi Respati <resir014@gmail.com>
2021-01-15 18:52:43 +0700

    refactor saweria alerts (still not working) (#51)

---
+++ next.config.js
@@ -0,0 +1,58 @@
+const { SAWERIA_API_URL } = process.env
+
+const nextConfig = {
+  target: 'server',
+  async headers() {
+    return [
+      {
+        source: '/_next/static/(.*)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable'
+          }
+        ]
+      },
+      {
+        source: '/static/(.*)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable'
+          }
+        ]
+      },
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY'
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block'
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff'
+          }
+        ]
+      }
+    ]
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: '/api/:path*'
+      },
+      {
+        source: '/api/saweria/:path*',
+        destination: `${SAWERIA_API_URL}/:path*`
+      }
+    ]
+  }
+}
+
+module.exports = nextConfig
