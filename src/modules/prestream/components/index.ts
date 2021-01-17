7251599de18e94dc33732e701a0d73c666dcb50d
Resi Respati <resir014@gmail.com>
2021-01-18 00:25:36 +0700

    remove legacy prestream components

--- src/modules/prestream/components/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as PrestreamChatWidget } from './PrestreamChatWidget'
-export { default as PrestreamContentBlock } from './PrestreamContentBlock'
-export { default as PrestreamFooterBlock } from './PrestreamFooterBlock'
-export { default as PrestreamRoot } from './PrestreamRoot'
-export { default as PrestreamSection } from './PrestreamSection'
