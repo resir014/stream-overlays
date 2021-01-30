2f1fbd46dcff0644961182424e197f716f2fc023
Resi Respati <resir014@gmail.com>
2021-01-30 16:24:46 +0700

    automated stream start progress bar (#60)

---
+++ src/modules/scenes/utils/parseStreamTimeQuery.ts
@@ -0,0 +1,11 @@
+import { ParsedUrlQuery } from 'querystring'
+import numberify from '~/utils/numberify'
+
+export default function parseStreamTimeQuery(query: ParsedUrlQuery) {
+  const { startH, startM } = query
+
+  return {
+    startH: numberify(startH) || 21,
+    startM: numberify(startM) || 0
+  }
+}
