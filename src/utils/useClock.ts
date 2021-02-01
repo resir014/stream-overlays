3a63b4937e1f588128a0654ce68b94613af67237
Resi Respati <resir014@gmail.com>
2021-02-01 14:43:33 +0700

    change useClock tick to 500ms

--- src/utils/useClock.ts
+++ src/utils/useClock.ts
@@ -2,19 +2,22 @@ import * as React from 'react'
 import { TaskTimer } from 'tasktimer'
 
 export default function useClock() {
-  const timer = new TaskTimer(1000)
+  const timer = new TaskTimer(500)
   const [time, setTime] = React.useState<Date>(new Date())
 
   const tick = () => {
     setTime(new Date())
   }
 
+  timer.add({
+    tickInterval: 1,
+    callback: tick
+  })
+
   React.useEffect(() => {
-    timer.on('tick', tick)
     timer.start()
 
     return () => {
-      timer.off('tick', tick)
       timer.stop()
     }
   }, [])
