d9b6dfcad83276362efcd83e39cfa8cb6106ccf0
Resi Respati <resir014@gmail.com>
2021-02-06 17:06:43 +0700

    add matchmaking rank mappings

--- src/modules/trackmania/matchmaking/MatchmakingBlock.tsx
+++ src/modules/trackmania/matchmaking/MatchmakingBlock.tsx
@@ -2,6 +2,7 @@ import { css } from '@emotion/react'
 import { Box } from '@resir014/chungking-react'
 import * as React from 'react'
 import MatchmakingDataItem from './MatchmakingDataItem'
+import matchmakingMap from './matchmakingMap'
 import useMatchmakingData from './useMatchmakingData'
 
 const MatchmakingBlock: React.FC = () => {
@@ -9,25 +10,33 @@ const MatchmakingBlock: React.FC = () => {
     apiUrl: '/api/trackmania/matchmaking-mmr'
   })
 
+  const currentRank = React.useMemo(() => {
+    if (results) {
+      const { score } = results[0]
+      const rank = matchmakingMap.filter(table => table.mmr <= score).pop()
+      return rank?.name
+    }
+
+    return undefined
+  }, [results])
+
   return (
-    <>
-      <Box
-        display="flex"
-        flexDirection="row"
-        p="xs"
-        borderTop="4px solid"
-        borderTopColor="blue.500"
-        css={css`
-          > :not(style) ~ :not(style) {
-            margin-inline-start: 8px;
-            margin-top: 0;
-          }
-        `}
-      >
-        <MatchmakingDataItem name="MMR" value={results?.[0].score || '???'} />
-        <MatchmakingDataItem name="RANK" value={results?.[0].rank || '???'} />
-      </Box>
-    </>
+    <Box
+      display="flex"
+      flexDirection="row"
+      p="xs"
+      borderTop="4px solid"
+      borderTopColor="blue.500"
+      css={css`
+        > :not(style) ~ :not(style) {
+          margin-inline-start: 8px;
+          margin-top: 0;
+        }
+      `}
+    >
+      <MatchmakingDataItem name="MMR" value={results?.[0].score || '???'} />
+      <MatchmakingDataItem name="RANK" value={currentRank || '???'} />
+    </Box>
   )
 }
 
---
+++ src/modules/trackmania/matchmaking/matchmakingMap.ts
@@ -0,0 +1,48 @@
+const matchmakingMap = [
+  {
+    name: 'U',
+    mmr: 0
+  },
+  {
+    name: 'B1',
+    mmr: 1000
+  },
+  {
+    name: 'B2',
+    mmr: 1250
+  },
+  {
+    name: 'B3',
+    mmr: 1500
+  },
+  {
+    name: 'C1',
+    mmr: 2000
+  },
+  {
+    name: 'C2',
+    mmr: 2250
+  },
+  {
+    name: 'C3',
+    mmr: 2500
+  },
+  {
+    name: 'M1',
+    mmr: 3000
+  },
+  {
+    name: 'M2',
+    mmr: 3500
+  },
+  {
+    name: 'M3',
+    mmr: 4000
+  },
+  {
+    name: 'TM',
+    mmr: 5000
+  }
+]
+
+export default matchmakingMap
