803aa0c5515c6103429792ff20f8c18c745b0e5c
Resi Respati <resir014@gmail.com>
2021-08-04 17:13:02 +0700

    migrate root elements to tailwind

--- src/components/dashboard/DashboardRoot.tsx
+++ src/components/dashboard/DashboardRoot.tsx
@@ -1,20 +1,9 @@
 import * as React from 'react'
-import { Box } from '@resir014/chungking-react'
 
 const DashboardRoot: React.FC = ({ children }) => (
-  <Box
-    as="main"
-    display="flex"
-    flexDirection="column"
-    width="100%"
-    height="100%"
-    minHeight="100vh"
-    overflow="hidden"
-    backgroundColor="black"
-    color="white"
-  >
+  <main className="flex flex-col w-full h-full min-h-screen overflow-hidden bg-chungking-black text-chungking-white">
     {children}
-  </Box>
+  </main>
 )
 
 export default DashboardRoot
--- src/components/overlay/OverlayRoot.tsx
+++ src/components/overlay/OverlayRoot.tsx
@@ -1,20 +1,7 @@
 import * as React from 'react'
-import { Box } from '@resir014/chungking-react'
-import { Global } from '@emotion/react'
-import overlayGlobalStyles from './utils/overlayGlobalStyles'
 
 const OverlayRoot: React.FC = ({ children }) => (
-  <Box
-    display="flex"
-    flexDirection="column"
-    width="100%"
-    height="100vh"
-    backgroundColor="transparent"
-    overflow="hidden"
-  >
-    <Global styles={overlayGlobalStyles} />
-    {children}
-  </Box>
+  <div className="flex flex-col w-full h-screen bg-transparent overflow-hidden">{children}</div>
 )
 
 export default OverlayRoot
