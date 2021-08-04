ec6b317ea1e94a13309a084026dd3e08550bb36f
Resi Respati <resir014@gmail.com>
2021-08-04 12:51:21 +0700

    fix purge config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@
 /** @type {import('tailwindcss/tailwind-config').TailwindConfig} */
 module.exports = {
   mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: ['./src/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   presets: [require('@resir014/tailwind-preset-chungking')],
   theme: {
