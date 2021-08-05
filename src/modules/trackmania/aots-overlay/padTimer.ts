74486464d93a89a7511a4155f0337810952ff450
Resi Respati <resir014@gmail.com>
2021-08-05 20:23:36 +0700

    remove unused widgets

--- src/modules/trackmania/aots-overlay/padTimer.ts
+++
@@ -1,5 +0,0 @@
-function padTimer(time: number) {
-  return time.toString().padStart(2, '0')
-}
-
-export default padTimer
--- src/modules/trackmania/telemetry/components/index.ts
+++
@@ -1,7 +0,0 @@
-export { default as BrakeIndicator } from './BrakeIndicator'
-export { default as ResetIndicator } from './ResetIndicator'
-export { default as SteeringIndicator } from './SteeringIndicator'
-export { default as TelemetryGridItem } from './TelemetryGridItem'
-export { default as ThrottleIndicator } from './ThrottleIndicator'
-
-export * from './TextStyles'
