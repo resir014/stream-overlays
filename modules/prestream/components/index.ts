cba542da980d6bf89dba0d07efb68c9487c938ee
Resi Respati <resir014@gmail.com>
2021-08-30 20:36:25 +0700

    remove old scenes files

--- modules/prestream/components/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as PrestreamDetails } from './PrestreamDetails';
-export { default as PrestreamEventsBlock } from './PrestreamEventsBlock';
-export { default as SceneInner } from './SceneInner';
