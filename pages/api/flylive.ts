5f6b3b41a62d962c85c4b532623b194254c7b9f6
Resi Respati <resir014@gmail.com>
2021-09-02 18:17:56 +0700

    fix tyoe errirs

--- pages/api/flylive.ts
+++ pages/api/flylive.ts
@@ -1,3 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-implicit-any-catch */
 import { NextApiRequest, NextApiResponse } from 'next';
 import fetch from 'isomorphic-unfetch';
 import parseFlyliveData from '~/lib/flightsim/parse-flylive-data';
@@ -10,7 +14,7 @@ const flylive = async (_: NextApiRequest, res: NextApiResponse) => {
     });
     const data = await response.text();
     res.status(200).json({ status: 'ok', data: parseFlyliveData(data) });
-  } catch (err) {
+  } catch (err: any) {
     res.status(500).json({
       status: 'error',
       data: {
