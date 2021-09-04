d68a5cb6ee395b4a2c239602f2ed72627de5cd81
Resi Respati <resir014@gmail.com>
2021-09-04 19:04:56 +0700

    migrate remaining blocks to tailwind (#102)

--- pages/blocks/capture-guard.tsx
+++ pages/blocks/capture-guard.tsx
@@ -1,4 +1,3 @@
-import { Box, Text } from '@resir014/chungking-react';
 import * as React from 'react';
 import { NextPage } from 'next';
 import { OverlayRoot } from '~/components/overlay';
@@ -6,19 +5,11 @@ import { OverlayRoot } from '~/components/overlay';
 const CaptureGuardBlock: NextPage = () => {
   return (
     <OverlayRoot>
-      <Box
-        display="flex"
-        alignItems="center"
-        justifyContent="center"
-        backgroundColor="black"
-        border="4px solid"
-        borderColor="green.500"
-        flex="1 1 auto"
-      >
-        <Text variant="4xl" display="inline-block" maxWidth={480} color="white" fontWeight={600}>
+      <div className="flex flex-auto items-center justify-center bg-chungking-black border-4 border-chungking-green-500">
+        <span className="inline-block max-w-[480px] text-4xl text-chungking-white font-semibold">
           if you&apos;re reading this then something bad happened
-        </Text>
-      </Box>
+        </span>
+      </div>
     </OverlayRoot>
   );
 };
