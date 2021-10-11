dd68a509d808fde72a50d0877920915848e9d9b5
Resi Respati <resir014@gmail.com>
2021-10-11 22:22:22 +0700

    move some color getters to utils folder

--- modules/pre-stream/pre-stream-header.tsx
+++ modules/pre-stream/pre-stream-header.tsx
@@ -2,29 +2,13 @@ import { useRouter } from 'next/router';
 import * as React from 'react';
 import { format } from 'date-fns';
 import clsx from 'clsx';
+import { getPrestreamAccentGradient } from './utils';
 import { parseStreamTimeQuery } from '~/lib/query-parser';
 import { usePrestreamClock } from '~/lib/pre-stream/use-prestream-clock';
 import { PreStreamVariants } from '~/lib/pre-stream/types';
 
 export interface PreStreamHeaderProps {
   variant?: PreStreamVariants;
-}
-
-function getPrestreamAccentColor(variant: PreStreamVariants) {
-  switch (variant) {
-    case 'pre-stream': {
-      return 'from-[rgba(26,36,156,0.75)] to-[rgba(36,136,245,0.75)]';
-    }
-    case 'brb': {
-      return 'from-[rgba(0,57,134,0.75)] to-[rgba(31,199,145,0.75)]';
-    }
-    case 'end': {
-      return 'from-[rgba(134,0,0,0.75)] to-[rgba(203,137,29,0.75)]';
-    }
-    default: {
-      return 'from-[rgba(26,36,156,0.75)] to-[rgba(36,136,245,0.75)]';
-    }
-  }
 }
 
 export function PreStreamHeader({ variant = 'pre-stream' }: PreStreamHeaderProps) {
@@ -41,7 +25,7 @@ export function PreStreamHeader({ variant = 'pre-stream' }: PreStreamHeaderProps
         <div
           className={clsx(
             'block w-4 h-4 rounded-full bg-gradient-to-b',
-            getPrestreamAccentColor(variant),
+            getPrestreamAccentGradient(variant),
           )}
         />
         <div className="flex flex-row space-x-4 text-3xl leading-none text-chungking-white">
