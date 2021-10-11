a15be3a17b442fcd8f0870731e599b7c652793d8
Resi Respati <resir014@gmail.com>
2021-10-11 20:31:49 +0700

    add upcoming stream API (#114)

--- lib/query-parser.ts
+++ lib/query-parser.ts
@@ -1,12 +1,47 @@
 import { ParsedUrlQuery } from 'querystring';
-import { numberify } from '~/lib/numberify';
+
+/**
+ * Converts a string-like value to a number.
+ *
+ * @param maybeString A string or an array of string
+ */
+export function parseNumber(maybeString?: string | string[]) {
+  if (maybeString) {
+    if (Array.isArray(maybeString)) {
+      const [num] = maybeString;
+      return Number(num);
+    }
+
+    return Number(maybeString);
+  }
+
+  return undefined;
+}
+
+/**
+ * Converts a string-like value to pure string.
+ *
+ * @param maybeString A string or an array of string
+ */
+export function parseString(maybeString?: string | string[]) {
+  if (maybeString) {
+    if (Array.isArray(maybeString)) {
+      const [string] = maybeString;
+      return string;
+    }
+
+    return maybeString;
+  }
+
+  return undefined;
+}
 
 export function parseStreamTimeQuery(query: ParsedUrlQuery) {
   const { startH, startM } = query;
 
   return {
-    startH: numberify(startH) ?? 21,
-    startM: numberify(startM) ?? 0,
+    startH: parseNumber(startH) ?? 21,
+    startM: parseNumber(startM) ?? 0,
   };
 }
 
@@ -14,7 +49,7 @@ export function parseTimeSignalQuery(query: ParsedUrlQuery) {
   const { h, m } = query;
 
   return {
-    h: numberify(h) ?? 21,
-    m: numberify(m) ?? 0,
+    h: parseNumber(h) ?? 21,
+    m: parseNumber(m) ?? 0,
   };
 }
---
+++ pages/api/notion/upcoming.ts
@@ -0,0 +1,95 @@
+import { NextApiHandler } from 'next';
+import { notion } from '~/lib/notion';
+import { parseNumber, parseString } from '~/lib/query-parser';
+
+// Gets the all upcoming streams based on the reference date/time.
+//
+// GET `/api/notion/upcoming`
+//
+// Query parameters:
+// - `reference_date`: an ISO 8601 compliant date/time string used for the `after`
+//   filter on the Notion API. Defaults to current date/time.
+// - `page_size`: maximum data rendered per API request. Defaults to `3`.
+
+const handler: NextApiHandler = async (req, res) => {
+  try {
+    if (req.method === 'GET') {
+      if (process.env.NOTION_DATABASE_ID) {
+        const { reference_date, page_size } = req.query;
+        const parsedDate = parseString(reference_date);
+        const parsedPageSize = parseNumber(page_size);
+
+        const { results } = await notion.databases.query({
+          database_id: process.env.NOTION_DATABASE_ID,
+          filter: {
+            property: 'Date',
+            date: {
+              after: parsedDate ? new Date(parsedDate).toISOString() : new Date().toISOString(),
+            },
+          },
+          sorts: [
+            {
+              property: 'Date',
+              direction: 'ascending',
+            },
+          ],
+          page_size: parsedPageSize ?? 3,
+        });
+
+        const data = results.map(({ id, properties }) => ({
+          id,
+          date: properties.Date.type === 'date' ? properties.Date.date.start : undefined,
+          series: properties.Series.type === 'select' ? properties.Series.select.name : undefined,
+          category:
+            properties.Category.type === 'multi_select'
+              ? properties.Category.multi_select.map(select => select.name)
+              : undefined,
+          stream_name:
+            properties['Stream Name'].type === 'title'
+              ? properties['Stream Name'].title[0].type === 'text'
+                ? properties['Stream Name'].title[0].text.content
+                : undefined
+              : undefined,
+          description:
+            properties.Description.type === 'rich_text'
+              ? properties.Description.rich_text[0].type === 'text'
+                ? properties.Description.rich_text[0].text.content
+                : undefined
+              : undefined,
+        }));
+
+        res.status(200).json({
+          status: 'ok',
+          data,
+        });
+      } else {
+        throw new Error('No database ID provided.');
+      }
+    } else {
+      res.status(404).json({
+        status: 'error',
+        data: {
+          message: 'Page not found.',
+        },
+      });
+    }
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      res.status(400).json({
+        status: 'error',
+        data: {
+          message: err.message,
+        },
+      });
+    } else {
+      res.status(400).json({
+        status: 'error',
+        data: {
+          message: 'An unknown error occured.',
+        },
+      });
+    }
+  }
+};
+
+export default handler;
