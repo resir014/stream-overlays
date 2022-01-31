faf616de2c1ef01a96b53889a40a2f7198640427
Resi Respati <resir014@gmail.com>
2022-01-31 18:54:30 +0700

    handle empty values properly in upcoming streams

--- pages/api/notion/upcoming.ts
+++ pages/api/notion/upcoming.ts
@@ -36,28 +36,32 @@ const handler: NextApiHandler = async (req, res) => {
           page_size: parsedPageSize ?? 3,
         });
 
-        const data = results.map(({ id, properties }) => ({
-          id,
-          date: properties.Date.type === 'date' ? properties.Date.date.start : undefined,
-          // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-          series: properties.Series.type === 'select' ? properties.Series.select?.name : undefined,
-          category:
-            properties.Category.type === 'multi_select'
-              ? properties.Category.multi_select.map(select => select.name)
-              : undefined,
-          stream_name:
-            properties['Stream Name'].type === 'title'
-              ? properties['Stream Name'].title[0].type === 'text'
-                ? properties['Stream Name'].title[0].text.content
-                : undefined
-              : undefined,
-          description:
-            properties.Description.type === 'rich_text'
-              ? properties.Description.rich_text[0].type === 'text'
-                ? properties.Description.rich_text[0].text.content
-                : undefined
-              : undefined,
-        }));
+        const data = results.map(({ id, properties }) => {
+          console.log(JSON.stringify({ id, properties }, null, 2));
+          return {
+            id,
+            date: properties.Date.type === 'date' ? properties.Date.date.start : undefined,
+            series:
+              // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+              properties.Series.type === 'select' ? properties.Series.select?.name : undefined,
+            category:
+              properties.Category.type === 'multi_select'
+                ? properties.Category.multi_select.map(select => select.name)
+                : undefined,
+            stream_name:
+              properties['Stream Name'].type === 'title'
+                ? properties['Stream Name'].title[0]?.type === 'text'
+                  ? properties['Stream Name'].title[0]?.text.content
+                  : undefined
+                : undefined,
+            description:
+              properties.Description.type === 'rich_text'
+                ? properties.Description.rich_text[0]?.type === 'text'
+                  ? properties.Description.rich_text[0]?.text.content
+                  : undefined
+                : undefined,
+          };
+        });
 
         res.status(200).json({
           status: 'ok',
