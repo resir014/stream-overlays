4b7fd8b5fd1f15c1b209b83feff41634b8561ded
Resi Respati <resir014@gmail.com>
2022-02-05 20:53:11 +0700

    last-minute countdown clock (#133)

---
+++ pages/blocks/countdown-timer.tsx
@@ -0,0 +1,42 @@
+import { useRouter } from 'next/router';
+import * as React from 'react';
+import Countdown from 'react-countdown';
+import { OverlayLayout } from '~/layouts/overlay-layout';
+import { createNextPage } from '~/lib/create-next-page';
+import { useOnMount } from '~/lib/hooks/use-on-mount';
+import { parseNumber } from '~/lib/query-parser';
+
+function CountdownTimerPage() {
+  const router = useRouter();
+  const [isCountdownRendered, setIsCountdownRendered] = React.useState(false);
+
+  useOnMount(() => {
+    setIsCountdownRendered(true);
+  });
+
+  const minutes = React.useMemo(
+    () => (parseNumber(router.query.minutes) ?? 1) * 60 * 1000,
+    [router.query],
+  );
+
+  return (
+    <div>
+      {isCountdownRendered && (
+        <Countdown
+          date={Date.now() + minutes}
+          precision={3}
+          renderer={props => (
+            <p className="block text-4xl font-bold text-white">
+              {props.minutes.toString().padStart(2, '0')}:
+              {props.seconds.toString().padStart(2, '0')}
+            </p>
+          )}
+        />
+      )}
+    </div>
+  );
+}
+
+export default createNextPage(CountdownTimerPage, {
+  layout: OverlayLayout,
+});
