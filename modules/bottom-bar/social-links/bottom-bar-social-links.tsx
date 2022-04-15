d60a1e0c08c730192bda7a8e6b73c62d31c6433e
Resi Respati <resir014@gmail.com>
2022-04-15 20:29:06 +0700

    fix social links transition

--- modules/bottom-bar/social-links/bottom-bar-social-links.tsx
+++ modules/bottom-bar/social-links/bottom-bar-social-links.tsx
@@ -1,53 +1,10 @@
 import * as React from 'react';
-import styled from '@emotion/styled';
-import { css } from '@emotion/react';
-import { Transition } from 'react-transition-group';
-import { TransitionStatus } from 'react-transition-group/Transition';
-
+import { Transition } from '@headlessui/react';
 import { socialLinks } from './social-links';
 import { useInterval } from '~/lib/hooks/use-interval';
 import sleep from '~/lib/sleep';
 
 export const TRANSITION_DURATION = 500;
-
-interface TransitionProps {
-  state: TransitionStatus;
-}
-
-const Exited = css`
-  opacity: 0;
-`;
-
-const Entered = css`
-  opacity: 1;
-`;
-
-const transitionStyles = ({ state }: TransitionProps) => {
-  switch (state) {
-    case 'entering': {
-      return Entered;
-    }
-    case 'entered': {
-      return Entered;
-    }
-    case 'exiting': {
-      return Exited;
-    }
-    case 'exited': {
-      return Exited;
-    }
-    default: {
-      return undefined;
-    }
-  }
-};
-
-const Container = styled('div')<TransitionProps>`
-  transition: all ${TRANSITION_DURATION}ms ease;
-  opacity: 0;
-
-  ${transitionStyles}
-`;
 
 export const BottomBarSocialLinks: React.FC = () => {
   const [currentIndex, setCurrentIndex] = React.useState(0);
@@ -69,12 +26,17 @@ export const BottomBarSocialLinks: React.FC = () => {
   }, 8000);
 
   return (
-    <Transition in={!transitioning} timeout={TRANSITION_DURATION}>
-      {state => (
-        <Container className="px-3 border-r-2 border-chungking-blue-500" state={state}>
-          {socialLinks[currentIndex]}
-        </Container>
-      )}
+    <Transition
+      show={!transitioning}
+      className="px-3 border-r-2 border-chungking-blue-500"
+      enter="transition-opacity duration-500"
+      enterFrom="opacity-0"
+      enterTo="opacity-100"
+      leave="transition-opacity duration-500"
+      leaveFrom="opacity-100"
+      leaveTo="opacity-0"
+    >
+      {socialLinks[currentIndex]}
     </Transition>
   );
 };
