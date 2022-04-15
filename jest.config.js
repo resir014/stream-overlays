baf3221941ae421b23945429d2a2e03415dcf9c6
Resi Respati <resir014@gmail.com>
2022-04-15 21:07:25 +0700

    fix tests

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,17 @@
-module.exports = {
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const jestConfig = {
   ...require('./test/jest-common'),
   collectCoverageFrom: [
     './(components|lib|pages)/**/*.(ts|tsx|js|jsx)',
     '!./(components|lib|pages)/**/__tests__/**/*.test.(ts|tsx|js|jsx)',
     '!./(components|lib|pages)/**/__mocks__/**/*.(ts|tsx|js|jsx)',
   ],
-  transform: {
-    '^.+\\.(ts|tsx|js|jsx)$': 'babel-jest',
-  },
   projects: ['./test/jest.client.js', './test/jest.server.js'],
 };
+
+module.exports = createJestConfig(jestConfig);
