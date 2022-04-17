94c71999c99be4bf91b4ddbc2eb3ba8b6f94bd63
Resi Respati <resir014@gmail.com>
2022-04-17 12:36:55 +0700

    scene wrapper refactoring

--- modules/scenes/scene-wrapper.tsx
+++ modules/scenes/scene-wrapper.tsx
@@ -1,16 +1,9 @@
 import * as React from 'react';
-import { BottomBar } from '../bottom-bar';
 
-export interface SceneWrapperProps {
-  showClock?: boolean;
-}
-
-export const SceneWrapper: React.FC<SceneWrapperProps> = ({ children, showClock }) => {
+export const SceneWrapper: React.FC = ({ children }) => {
   return (
     <div className="grid grid-rows-scene-wrapper grid-cols-1 flex-1 w-full min-w-[450px] relative bg-chungking-black bg-opacity-75">
       <div className="flex flex-col">{children}</div>
-      <div className="bg-chungking-grey-900" />
-      <BottomBar variant={showClock ? 'default' : 'prestream'} />
     </div>
   );
 };
