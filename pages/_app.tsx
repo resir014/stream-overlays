44eb670bb3ac82fe9384d281f903d6c0d5c02a7f
Resi Respati <resir014@gmail.com>
2022-04-18 14:41:35 +0700

    changed favicon + social images

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,6 +14,10 @@ export default function MyApp({ Component, pageProps }: NextAppProps): JSX.Eleme
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="shortcut icon" href="/favicon.ico" />
       </Head>
       {page}
     </>
