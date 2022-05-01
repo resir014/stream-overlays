2ab45538e7a51a12a13af5b6c80de6b317321dfc
Resi Respati <resir014@gmail.com>
2022-05-01 08:17:25 +0700

    overlay 2.1 (#140)

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,11 +13,11 @@ module.exports = {
     extend: {
       gridTemplateRows: {
         'bottom-bar': '40px 24px',
-        'scene-wrapper': '1fr 56px 64px',
+        'scene-wrapper': '1fr 44px 64px',
       },
       gridTemplateColumns: {
         'bottom-bar': 'auto 320px',
-        'pre-stream': '1fr 360px',
+        'pre-stream': '320px 84px auto 416px',
       },
       transitionTimingFunction: {
         'in-out-alerts': 'cubic-bezier(0.4, 0, 0.2, 1)',
