cdc2c53c6e01b5d240999caac9366a58c5ef3abb
Resi Respati <resir014@gmail.com>
2022-07-23 23:48:26 +0700

    h

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -28,9 +28,9 @@ function MyApp({ Component, pageProps }: NextAppProps): JSX.Element {
 
 export default withTRPC<AppRouter>({
   config() {
-    const host = process.env.VERCEL_URL ?? process.env.NEXT_PUBLIC_VERCEL_URL;
-    console.log(host);
-    const url = host ? `https://${host}/api/trpc` : 'http://localhost:3000/api/trpc';
+    const url = process.env.VERCEL_URL
+      ? `https://${process.env.VERCEL_URL}/api/trpc`
+      : 'http://localhost:3000/api/trpc';
 
     return {
       url,
