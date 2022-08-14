9dbe3a22eaf27ee2aba0116cf9bff503bc45f75d
Resi Respati <resir014@gmail.com>
2022-08-14 18:36:19 +0700

    fix: add jetbrains mono font

--- styles/fonts.tsx
+++ styles/fonts.tsx
@@ -1,2 +1,20 @@
 // Inter
 import 'typeface-inter';
+
+// JetBrains Mono
+import '@fontsource/jetbrains-mono/100.css';
+import '@fontsource/jetbrains-mono/100-italic.css';
+import '@fontsource/jetbrains-mono/200.css';
+import '@fontsource/jetbrains-mono/200-italic.css';
+import '@fontsource/jetbrains-mono/300.css';
+import '@fontsource/jetbrains-mono/300-italic.css';
+import '@fontsource/jetbrains-mono/400.css';
+import '@fontsource/jetbrains-mono/400-italic.css';
+import '@fontsource/jetbrains-mono/500.css';
+import '@fontsource/jetbrains-mono/500-italic.css';
+import '@fontsource/jetbrains-mono/600.css';
+import '@fontsource/jetbrains-mono/600-italic.css';
+import '@fontsource/jetbrains-mono/700.css';
+import '@fontsource/jetbrains-mono/700-italic.css';
+import '@fontsource/jetbrains-mono/800.css';
+import '@fontsource/jetbrains-mono/800-italic.css';
