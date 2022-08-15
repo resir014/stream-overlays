964194f819b7ee7f9af8d73b572b11220084cf1e
Resi Respati <resir014@gmail.com>
2022-08-15 17:41:10 +0700

    remove custom bottom bar variants

--- modules/bottom-bar/bottom-bar.tsx
+++ modules/bottom-bar/bottom-bar.tsx
@@ -1,23 +1,13 @@
 import * as React from 'react';
-import { BottomBarSocialLinks } from './social-links';
 import { BottomBarEvents } from './bottom-bar-events';
 import { BottomBarClock } from './bottom-bar-clock';
 
-export type BottomBarVariants = 'default' | 'prestream' | 'nye';
-
-export interface BottomBarProps {
-  variant?: BottomBarVariants;
-}
-
-export const BottomBar: React.FC<BottomBarProps> = ({ variant }) => {
-  const hideClock = React.useMemo(() => variant === 'prestream', [variant]);
-  const nyeVariant = React.useMemo(() => variant === 'nye', [variant]);
-
+export const BottomBar: React.FC = () => {
   return (
-    <div className="grid grid-cols-bottom-bar w-full h-full text-chungking-white bg-chungking-black border-t-2 border-chungking-white bg-opacity-90 max-h-[64px]">
+    <div className="grid grid-cols-bottom-bar w-full h-full text-chungking-white bg-chungking-black border-t-2 border-chungking-white max-h-[64px]">
       <BottomBarEvents />
       <div className="flex items-center justify-end px-12">
-        {hideClock || nyeVariant ? <BottomBarSocialLinks /> : <BottomBarClock />}
+        <BottomBarClock />
       </div>
     </div>
   );
