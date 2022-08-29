6770fea8556af8e65664f5794d4d33e5c81e4d91
Resi Respati <resir014@gmail.com>
2022-08-29 18:37:26 +0700

    feat: add studio clock overlay for OBS dock

---
+++ pages/blocks/studio-clock-obs.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import { OverlayLayout } from '~/layouts/overlay-layout';
+import { createNextPage } from '~/lib/create-next-page';
+import { useOnMount } from '~/lib/hooks/use-on-mount';
+import { StudioClockInterface } from '~/modules/studio-clock/studio-clock-interface';
+
+function StudioClockOBSSourcePage() {
+  const [isClockRendered, setIsClockRendered] = React.useState(false);
+
+  useOnMount(() => {
+    setIsClockRendered(true);
+  });
+
+  return (
+    <div className="flex w-full h-screen items-center justify-center bg-chungking-black">
+      {isClockRendered ? (
+        <div className="scale-50">
+          <StudioClockInterface hideTimezone />
+        </div>
+      ) : null}
+    </div>
+  );
+}
+
+export default createNextPage(StudioClockOBSSourcePage, {
+  layout: OverlayLayout,
+});
