1be57c8cf59a806cb4be7d3a77464295d9815158
Resi Respati <resir014@gmail.com>
2022-09-26 17:51:12 +0700

    feat: studio clock scene

--- modules/scenes/scene-wrapper.tsx
+++ modules/scenes/scene-wrapper.tsx
@@ -1,8 +1,25 @@
+import clsx from 'clsx';
 import * as React from 'react';
 
-export const SceneWrapper: React.FC = ({ children }) => {
+export interface SceneWrapperProps extends React.HTMLProps<HTMLDivElement> {
+  darkBackground?: boolean;
+}
+
+export const SceneWrapper: React.FC<SceneWrapperProps> = ({
+  children,
+  className,
+  darkBackground,
+  ...rest
+}) => {
   return (
-    <div className="grid grid-rows-scene-wrapper grid-cols-1 flex-1 w-full min-w-[450px] relative bg-chungking-black/75">
+    <div
+      className={clsx(
+        'grid grid-rows-scene-wrapper grid-cols-1 flex-1 w-full min-w-[450px] relative',
+        darkBackground ? 'bg-chungking-black' : 'bg-chungking-black/75',
+        className,
+      )}
+      {...rest}
+    >
       {children}
     </div>
   );
