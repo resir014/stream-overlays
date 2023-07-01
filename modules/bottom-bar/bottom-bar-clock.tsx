57e40e86ee06ff264630282bd7036c35e8f62d8b
Resi Respati <resir014@gmail.com>
2023-07-01 14:05:37 +0700

    deps upgrade (2023-07-01) (#163)

--- modules/bottom-bar/bottom-bar-clock.tsx
+++ modules/bottom-bar/bottom-bar-clock.tsx
@@ -1,11 +1,11 @@
-import { createStyleString } from '@capsizecss/core';
+import { createStyleObject } from '@capsizecss/core';
 import fontMetrics from '@capsizecss/metrics/inter';
 import { format } from 'date-fns';
 import * as React from 'react';
 import { useClock } from '~/lib/hooks/use-clock';
 import { useOnMount } from '~/lib/hooks/use-on-mount';
 
-const styles = createStyleString('capsized-text', {
+const styles = createStyleObject({
   capHeight: 18,
   lineGap: 0,
   fontMetrics,
@@ -23,7 +23,7 @@ export function BottomBarClock() {
     if (mounted) {
       return (
         <div className="flex items-center justify-center w-[96px] h-[32px] py-1 text-chungking-black bg-chungking-white rounded-xl leading-none font-bold">
-          <span className="capsized-text">{format(time, 'HH:mm')}</span>
+          <span style={styles}>{format(time, 'HH:mm')}</span>
         </div>
       );
     }
