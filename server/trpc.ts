608bfab7b657e2ae9f18c996670c4abf8c8c0963
Resi Respati <resir014@gmail.com>
2023-07-01 19:58:30 +0700

    remove unused comment

--- server/trpc.ts
+++ server/trpc.ts
@@ -11,10 +11,7 @@ const t = initTRPC.context().create({
     };
   },
 });
-/**
- * We recommend only exporting the functionality that we
- * use so we can enforce which base procedures should be used
- **/
+
 export const router = t.router;
 export const mergeRouters = t.mergeRouters;
 export const publicProcedure = t.procedure;
