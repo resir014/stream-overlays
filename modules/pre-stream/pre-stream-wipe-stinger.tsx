1c476d1b3613d4669ac4ecbbfcff513928aaa905
Resi Respati <resir014@gmail.com>
2023-09-14 02:01:30 +0700

    add logo

--- modules/pre-stream/pre-stream-wipe-stinger.tsx
+++ modules/pre-stream/pre-stream-wipe-stinger.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { Transition } from '@headlessui/react';
 import { useClock } from '~/lib/hooks/use-clock';
 import { useOverlayData } from '../overlay-data/use-overlay-data';
+import Image from 'next/image';
 
 export function PreStreamWipeStinger() {
   const time = useClock();
@@ -24,7 +25,7 @@ export function PreStreamWipeStinger() {
 
   return (
     <div className="absolute top-0 left-0 w-full h-full z-10">
-      <Transition show={isVisible}>
+      <Transition show={isVisible} className="relative w-full h-full">
         <Transition.Child
           as={React.Fragment}
           enter="transition ease-in-out duration-500 transform"
@@ -48,6 +49,20 @@ export function PreStreamWipeStinger() {
           enterTo="opacity-100 translate-x-0"
         >
           <div className="absolute bg-chungking-blue-900 w-[1280px] h-full shadow-drop-layers" />
+        </Transition.Child>
+        <Transition.Child
+          as={React.Fragment}
+          enter="transition ease-in-out duration-500 transform delay-500"
+          enterFrom="opacity-0 scale-0"
+          enterTo="opacity-100 scale-100"
+        >
+          <Image
+            className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] mt-[48px] rounded-full"
+            src="/static/resir014-logo-2023.png"
+            alt="resir014 logo"
+            width={256}
+            height={256}
+          />
         </Transition.Child>
       </Transition>
     </div>
