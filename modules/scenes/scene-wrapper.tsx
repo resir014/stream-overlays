c7504d3128b67b134fd4bcd0c0a2421b74fc1054
Resi Respati <resir014@gmail.com>
2023-09-26 00:29:44 +0700

    update studio clock UI

--- modules/scenes/scene-wrapper.tsx
+++ modules/scenes/scene-wrapper.tsx
@@ -3,19 +3,21 @@ import * as React from 'react';
 
 export interface SceneWrapperProps extends React.HTMLProps<HTMLDivElement> {
   darkBackground?: boolean;
+  transparent?: boolean;
 }
 
 export function SceneWrapper({
   children,
   className,
   darkBackground,
+  transparent,
   ...rest
 }: React.PropsWithChildren<SceneWrapperProps>) {
   return (
     <div
       className={clsx(
         'flex flex-col flex-1 w-full min-w-[450px] relative',
-        darkBackground ? 'bg-chungking-black' : 'bg-chungking-black/50',
+        transparent ? undefined : darkBackground ? 'bg-chungking-black' : 'bg-chungking-black/50',
         className,
       )}
       {...rest}
--- pages/scenes/studio-clock.tsx
+++ pages/scenes/studio-clock.tsx
@@ -14,12 +14,12 @@ function StudioClockScenePage() {
   });
 
   return (
-    <SceneWrapper darkBackground>
+    <SceneWrapper transparent className="bg-chungking-black/90">
       <div className="grid grid-rows-scene-wrapper grid-cols-1 flex-1">
         <div className="flex flex-col items-center justify-center p-12 space-y-6">
           {isClockRendered ? (
             <div className="flex items-center justify-center w-[672px] h-[672px]">
-              <StudioClockInterface hideTimezone className="scale-150" />
+              <StudioClockInterface hideTimezone transparent className="scale-150" />
             </div>
           ) : null}
           {isClockRendered ? (
