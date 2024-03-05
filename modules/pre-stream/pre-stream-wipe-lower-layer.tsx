72e06b105e549fab311ba4b01ef35e09fd76e5f3
Resi Respati <resir014@gmail.com>
2024-03-05 20:39:45 +0700

    fix: adjust lower layer wipe initial size

--- modules/pre-stream/pre-stream-wipe-lower-layer.tsx
+++ modules/pre-stream/pre-stream-wipe-lower-layer.tsx
@@ -13,16 +13,16 @@ export function PreStreamWipeLowerLayer({
 }: PreStreamWipeLowerLayerProps) {
   return (
     <>
-      <div className={clsx('absolute w-full h-full -translate-x-[90%] z-0', className)} />
+      <div className={clsx('absolute w-full h-full -translate-x-[97.5%] z-0', className)} />
       <Transition show={isVisible} as={React.Fragment}>
         <Transition.Child
           as={React.Fragment}
           enter="transition ease-in-out duration-700 transform"
-          enterFrom="-translate-x-[90%]"
+          enterFrom="-translate-x-[97.5%]"
           enterTo="translate-x-0 shadow-drop-layers"
           unmount={false}
         >
-          <div className={clsx('absolute w-full h-full -translate-x-[90%] z-10', className)} />
+          <div className={clsx('absolute w-full h-full -translate-x-[97.5%] z-10', className)} />
         </Transition.Child>
       </Transition>
     </>
