ece52cf996259d555d31a14dc522d549c10f0980
Resi Respati <resir014@gmail.com>
2024-11-18 23:43:44 +0700

    fix: use uppercase route names

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -2,7 +2,7 @@ import { fetchRequestHandler } from '@trpc/server/adapters/fetch';
 import type { APIRoute } from 'astro';
 import { appRouter } from '~/server/routers/_app';
 
-export const all: APIRoute = opts => {
+export const ALL: APIRoute = opts => {
   return fetchRequestHandler({
     endpoint: '/api/trpc',
     req: opts.request,
