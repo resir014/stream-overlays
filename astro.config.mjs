1a3cd51239353ba93d1f678f78cbc482bcf714da
Resi Respati <resir014@gmail.com>
2024-11-18 22:08:19 +0700

    chore: add netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
-import tailwind from '@astrojs/tailwind';
-
+import netlify from '@astrojs/netlify';
 import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: netlify(),
   integrations: [tailwind(), react()],
 });
